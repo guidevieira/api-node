3924aea95c204564b4af65d08cde4a09d27e9e8f
guilherme.quintino <guilherme.quintino@systemhope.com.br>
2019-12-26 20:21:01 -0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql2')
 const fs = require('fs')
-
+console.log('aki')
 const port = process.env.PORT || 3000
 const connection = mysql.createConnection({
   host: 'mysql669.umbler.com',
@@ -19,32 +19,12 @@ connection.connect(err => {
   if (!err) {
     console.log('connected to db.')
   }
-  connection.query(`show tables like 'contacts'`, (err, rows) => {
-    if (rows.length === 0) {
-      const sql = String(fs.readFileSync('./db.sql'))
-      connection.query(sql, (err) => {
-        if (!err) {
-          console.log('database created.')
-        }
-      })
-    }
-  })
 })
 
 app.get('/', (req, res) => {
   res.send('Hello DevPleno!')
 })
-app.get('/contacts', (req, res) => {
-  connection.query('select * from contacts', (err, rows) => {
-    if (err) {
-      res.send({
-        error: 'error connecting to db'
-      })
-    } else {
-      res.send(rows)
-    }
-  })
-})
+
 app.listen(port, err => {
   if (!err) {
     console.log('server listening on port', port)
